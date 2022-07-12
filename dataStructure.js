07900b9ee90a632a9a3cee0785896bc302e2cdc9
viviankwok <104907963+viviankwok@users.noreply.github.com>
2022-07-12 17:32:52 +0800

    proposal meeting notes

---
+++ dataStructure.js
@@ -0,0 +1,41 @@
+//  DATA STRUCTURE
+
+// db: thisisatracker / collection: recipes
+const recipesData = {
+  // default ID in mongoDB
+  id: "5a18c6e82737dc8b317a46dc",
+  name: "avocado sandwhich",
+  meat: "beef",
+  veg: false,
+  calories: 0,
+  carb: "rice",
+  // vegetarian properly; boolean
+  vegetarian: true,
+  instructions: "preparation instructions",
+  // number in minutes
+  preparationTime: 20,
+  flavourProfile: ["spicy", "sweet", "savory", "unami"],
+};
+
+// db: thisisatracker / collection: users
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    hash: {
+      type: String,
+      required: true,
+    },
+  },
+
+  // storage location
+  { collection: "users" }
+);
+
+const User = mongoose.model("User", UserSchema);
+module.exports = User;
