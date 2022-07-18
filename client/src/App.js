26f2b4c34512f4ea33d77879e1d6d80285f502df
Benedict-Sim <crossfitjovi@gmail.com>
2022-07-18 14:32:38 +0800

    Frontend initial files

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Route, Navigate, Routes } from "react-router-dom";
+import NavBar from "./components/NavBars";
+import Main from "./components/Main";
+import Recipes from "./components/Recipes";
+import CalorieTracker from "./components/CalorieTracker";
+
+function App() {
+  return (
+    <div>
+      <h2>Recipe Tracker</h2>
+    </div>
+  );
+}
+
+export default App;
