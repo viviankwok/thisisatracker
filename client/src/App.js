e1c0641de0ac91fb53efe39ab3c5fa6f48f7363d
Benedict-Sim <crossfitjovi@gmail.com>
2022-07-20 14:54:13 +0800

    Update - Protected Routes, Login, View

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Route, Navigate, Routes } from "react-router-dom";
+import RequireLogin from "./components/RequireLogin";
 import NavBar from "./components/NavBar";
 import Main from "./components/Main";
 import Recipes from "./components/Recipes";
@@ -18,8 +19,12 @@ function App() {
           <Route path="/" element={<Navigate replace to="/Main" />} />
           <Route path="/Main" element={<Main />} />
           <Route path="/Login" element={<Login />} />
-          <Route path="/Recipes" element={<Recipes />} />
-          <Route path="/CalorieTracker" element={<CalorieTracker />} />
+
+          {/* Protect these routes with RequireLogin */}
+          <Route element={<RequireLogin />}>
+            <Route path="/Recipes" element={<Recipes />} />
+            <Route path="/CalorieTracker" element={<CalorieTracker />} />
+          </Route>
         </Routes>
       </main>
     </div>
---
+++ client/src/components/RequireLogin.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import { useLocation, Navigate, Outlet } from "react-router-dom";
+
+const RequireLogin = () => {
+  const storedLogin = localStorage.getItem("loginAccess");
+  const [accessToken, setAccessToken] = useState(storedLogin);
+
+  console.log(accessToken);
+  const location = useLocation();
+  return (
+    <div>
+      {accessToken}
+      {accessToken ? (
+        <Outlet />
+      ) : (
+        <Navigate to="/Login" state={{ from: location }} replace />
+      )}
+    </div>
+  );
+};
+
+export default RequireLogin;
